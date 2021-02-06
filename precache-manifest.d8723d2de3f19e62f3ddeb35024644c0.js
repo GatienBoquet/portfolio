f2a3d80dc8797c1c22517cdcb987c233327d78c6
Gatien Boquet <GatienBoquet@users.noreply.github.com>
2021-02-05 19:18:49 -0500

    Updates

---
+++ precache-manifest.d8723d2de3f19e62f3ddeb35024644c0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87386cbdae98e92d38aaabb2ad135125",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f160214ae6a67888cb68",
+    "url": "/portfolio/static/css/main.87a5c148.chunk.css"
+  },
+  {
+    "revision": "1578878dbdd3a8cc6331",
+    "url": "/portfolio/static/js/2.65560290.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.65560290.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f160214ae6a67888cb68",
+    "url": "/portfolio/static/js/main.3a4a849a.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "4f9d12fec5740ce67b67b81607cb84c2",
+    "url": "/portfolio/static/media/DM-Logo-sm.4f9d12fe.png"
+  },
+  {
+    "revision": "a671f156ba9a4443be0bbb097d69e315",
+    "url": "/portfolio/static/media/logo.a671f156.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.0453255eb501d6fde42013af2350a753.js"
+  "/portfolio/precache-manifest.d8723d2de3f19e62f3ddeb35024644c0.js"
 );
 
 self.addEventListener('message', (event) => {
